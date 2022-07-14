e12c9824fd56673c7136c488678037a2a5c7f256
Wesley Araujo <wparaujo7@gmail.com>
2022-07-13 23:00:14 -0300

    First commit

---
+++ BookStore_WesleyPereira/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookStore_WesleyPereira
+//
+//  Created by Wesley Araujo on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
