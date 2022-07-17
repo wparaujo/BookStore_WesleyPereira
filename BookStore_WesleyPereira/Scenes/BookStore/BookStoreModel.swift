6a280723ea3efa73a0dae0af8acadcbba4979c4c
Wesley Araujo <wparaujo7@gmail.com>
2022-07-17 12:50:38 -0300

    Adjustment in book store model

--- BookStore_WesleyPereira/Scenes/BookStore/BookStoreModel.swift
+++ BookStore_WesleyPereira/Scenes/BookStore/BookStoreModel.swift
@@ -7,49 +7,171 @@
 
 import Foundation
 
-struct BookStore: Codable {
+// MARK: - BookStoreModel
+struct BookStoreModel: Codable {
     let kind: String
     let totalItems: Int
     let items: [Item]
 }
 
+// MARK: - Item
 struct Item: Codable {
     let kind: String
-    let id: String
-    let etag: String
+    let id, etag: String
     let selfLink: String
     let volumeInfo: VolumeInfo
     let saleInfo: SaleInfo
     let accessInfo: AccessInfo
-    let searchInfo: SearchInfo
+    let searchInfo: SearchInfo?
 }
 
-struct VolumeInfo: Codable {
-    let title: String
-    let subtitle: String
-    let authors: [String]
-}
-
-struct SaleInfo: Codable {
-    let country: String
-    let saleability: String
-    let isBook: Bool
-}
-
+// MARK: - AccessInfo
 struct AccessInfo: Codable {
-    let country, viewability: String
+    let country: Country
+    let viewability: Viewability
     let embeddable, publicDomain: Bool
-    let textToSpeechPermission: String
+    let textToSpeechPermission: TextToSpeechPermission
     let epub, pdf: Epub
     let webReaderLink: String
-    let accessViewStatus: String
+    let accessViewStatus: AccessViewStatus
     let quoteSharingAllowed: Bool
 }
 
-struct Epub: Codable {
-    let isAvailable: Bool
+enum AccessViewStatus: String, Codable {
+    case none = "NONE"
+    case sample = "SAMPLE"
 }
 
+enum Country: String, Codable {
+    case br = "BR"
+}
+
+// MARK: - Epub
+struct Epub: Codable {
+    let isAvailable: Bool
+    let acsTokenLink: String?
+}
+
+enum TextToSpeechPermission: String, Codable {
+    case allowed = "ALLOWED"
+    case allowedForAccessibility = "ALLOWED_FOR_ACCESSIBILITY"
+}
+
+enum Viewability:String, Codable {
+    case noPages = "NO_PAGES"
+    case partial = "PARTIAL"
+}
+
+enum Kind: Codable {
+    case booksVolume
+}
+
+// MARK: - SaleInfo
+struct SaleInfo: Codable {
+    let country: Country
+    let saleability: Saleability
+    let isEbook: Bool
+    let listPrice, retailPrice: SaleInfoListPrice?
+    let buyLink: String?
+    let offers: [Offer]?
+}
+
+// MARK: - SaleInfoListPrice
+struct SaleInfoListPrice: Codable {
+    let amount: Double
+    let currencyCode: String
+}
+
+// MARK: - Offer
+struct Offer: Codable {
+    let finskyOfferType: Int
+    let listPrice, retailPrice: OfferListPrice
+    let giftable: Bool
+}
+
+// MARK: - OfferListPrice
+struct OfferListPrice: Codable {
+    let amountInMicros: Int
+    let currencyCode: String
+}
+
+enum Saleability: String, Codable {
+    case forSale = "FOR_SALE"
+    case notForSale = "NOT_FOR_SALE"
+}
+
+// MARK: - SearchInfo
 struct SearchInfo: Codable {
     let textSnippet: String
+}
+
+// MARK: - VolumeInfo
+struct VolumeInfo: Codable {
+    let title: String
+    let subtitle: String?
+    let authors: [String]
+    let publisher: String?
+    let publishedDate: String
+    let volumeInfoDescription: String?
+    let industryIdentifiers: [IndustryIdentifier]?
+    let readingModes: ReadingModes
+    let pageCount: Int
+    let printType: PrintType
+    let categories: [Category]
+    let maturityRating: MaturityRating
+    let allowAnonLogging: Bool
+    let contentVersion: String
+    let panelizationSummary: PanelizationSummary?
+    let imageLinks: ImageLinks
+    let language: Language
+    let previewLink: String
+    let infoLink: String
+    let canonicalVolumeLink: String
+    let averageRating: Float?
+    let ratingsCount: Int?
+}
+
+enum Category: String, Codable {
+    case computers = "Computers"
+    case deepDiving = "Deep diving"
+}
+
+// MARK: - ImageLinks
+struct ImageLinks: Codable {
+    let smallThumbnail, thumbnail: String
+}
+
+// MARK: - IndustryIdentifier
+struct IndustryIdentifier: Codable {
+    let type: String
+    let identifier: String
+}
+
+enum TypeEnum: Codable {
+    case isbn10
+    case isbn13
+    case other
+}
+
+enum Language: String, Codable {
+    case en
+    case ptBR = "pt-BR"
+}
+
+enum MaturityRating: String, Codable {
+    case notMature = "NOT_MATURE"
+}
+
+// MARK: - PanelizationSummary
+struct PanelizationSummary: Codable {
+    let containsEpubBubbles, containsImageBubbles: Bool
+}
+
+enum PrintType: String, Codable {
+    case book = "BOOK"
+}
+
+// MARK: - ReadingModes
+struct ReadingModes: Codable {
+    let text, image: Bool
 }
